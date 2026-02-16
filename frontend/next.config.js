54cd705688bafaffa8881990b09d8e831fdc6d76
RamzanAsghar11 <priceramzan9@gmail.com>
2026-02-17 00:56:58 +0500

    error solve

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    turbo: {
-      resolveAlias: {
-        '@': './src',
-      },
-    },
-  },
-}
+const nextConfig = {}
 
 module.exports = nextConfig
